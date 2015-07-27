f4044b31a5cf7e1b0f30755f78a0b046c6e33b17
rmisio <rmisio@users.noreply.github.com>
2015-07-27 09:23:27 -0700

    Commenting the base view

--- app/scripts/views/BaseView.js
+++ app/scripts/views/BaseView.js
@@ -17,6 +17,11 @@ define([
       }, this);
     },
 
+    // If you are creating child views within your view, call this method
+    // to register them. This will ensure that they will have their remove
+    // method called if the parent is removed. It will also ensure
+    // that they will have an attach event fired upon the parent
+    // being attached to the DOM (using the attach method).
     registerChild: function (childView) {
       if (this._childViews.indexOf(childView) === -1) {
         this._childViews.push(childView);
@@ -24,6 +29,11 @@ define([
       }
     },
 
+    // Opposite of registerChild. This method is automatically
+    // called by remove. For all practical purposes, you probably
+    // won't need to call this method directly (as long as you
+    // are remembering to call this base class's remove when you
+    // are overriding remove in your own views).
     unregisterChild: function (childView) {
       var name = '';
       if (childView.options && childView.options.name) {
@@ -38,6 +48,7 @@ define([
       }
     },
 
+    // Will call the remove method of any child views.
     remove: function () {
       for (var i=0; i < this._childViews.length; i++) {
         // no need to unregister child from parent,
@@ -53,6 +64,11 @@ define([
       Backbone.View.prototype.remove.apply(this, arguments);
     },
 
+    // Call this method to attach the view to the DOM. Using this
+    // method will ensure that an 'attach' will be fired for this
+    // view, as well as any child views. By default, render will
+    // be called prior to the $el being attached. To skip rendering,
+    // pass in false for options.render.
     attach: function ($container, options) {
       if (!$container || !$container.length) {
         throw new Error('Provide a $container to attach the view into.');
